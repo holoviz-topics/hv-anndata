05ecbc6c3e1d24668f27d6b98c6cadb07470f402
Philipp A. <flying-sheep@web.de>
2024-12-13 11:57:58 +0100

    Setup basic package (#1)

--- src/hv_anndata/__init__.py
+++ src/hv_anndata/__init__.py
@@ -1,10 +1,7 @@
-import importlib.metadata
-import warnings
+"""Anndata interface for holoviews."""
 
-try:
-    __version__ = importlib.metadata.version(__name__)
-except importlib.metadata.PackageNotFoundError as e:  # pragma: no cover
-    warnings.warn(f"Could not determine version of {__name__}\n{e!s}", stacklevel=2)
-    __version__ = "unknown"
+from __future__ import annotations
 
-__all__ = []  # <- IMPORTANT FOR DOCS: fill with imports
+from .interface import AnnDataInterface, register
+
+__all__ = ["AnnDataInterface", "register"]
---
+++ tests/test_basic.py
@@ -0,0 +1,7 @@
+"""Basic tests."""
+
+from __future__ import annotations
+
+
+def test_basic() -> None:
+    pass
