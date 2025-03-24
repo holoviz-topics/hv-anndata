b5439f8b8249ea17296e8271a44becd248377eae
Demetris Roumis <roumis.d@gmail.com>
2025-03-24 16:21:57 -0700

    add featuremap app

--- src/hv_anndata/__init__.py
+++ src/hv_anndata/__init__.py
@@ -4,5 +4,6 @@ from __future__ import annotations
 
 from .interface import AnnDataInterface, register
 from .plotting import Dotmap
+from .featuremap import FeatureMapApp, create_featuremap_plot
 
-__all__ = ["AnnDataInterface", "Dotmap", "register"]
+__all__ = ["AnnDataInterface", "Dotmap", "register", "FeatureMapApp", "create_featuremap_plot"]
