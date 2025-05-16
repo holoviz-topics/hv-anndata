a98935bb856ba5fc4afd240d5b86db684d5a7865
maximlt <mliquet@anaconda.com>
2025-05-16 14:08:09 +0200

    add tests

--- tests/test_manifold.py
+++ tests/test_manifold.py
@@ -6,12 +6,17 @@ from unittest.mock import Mock, patch
 
 import anndata as ad
 import colorcet as cc
+import holoviews as hv
 import numpy as np
 import pandas as pd
 import panel as pn
 import pytest
 
-from hv_anndata.manifoldmap import ManifoldMap, create_manifoldmap_plot
+from hv_anndata.manifoldmap import (
+    ManifoldMap,
+    create_manifoldmap_plot,
+    labeller,
+)
 
 
 @pytest.fixture
@@ -211,3 +216,28 @@ def test_manifoldmap_panel_layout(sadata: ad.AnnData) -> None:
 
     assert isinstance(layout, pn.layout.Row)
     assert len(layout) == 2
+
+
+def test_labeller() -> None:
+    df = pd.DataFrame(  # noqa: PD901
+        {
+            "UMAP1": [0, 1, 2, 3, 10],
+            "UMAP2": [0, 1, 2, 3, 10],
+            "cell_type": ["a", "a", "b", "b", "b"],
+        }
+    )
+    dataset = hv.Dataset(df, kdims=["UMAP1", "UMAP2"], vdims=("cell_type"))
+    ldm = labeller(dataset, min_count=0)
+    labels = ldm[()]
+    expected_data = pd.DataFrame(
+        {
+            "cell_type": ["b", "a"],
+            "count": [3, 2],
+            "x": [5, 0.5],
+            "y": [5, 0.5],
+        }
+    )
+    pd.testing.assert_frame_equal(
+        labels.data.sort_values("cell_type"),
+        expected_data.sort_values("cell_type"),
+    )
