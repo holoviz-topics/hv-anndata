84a9bfab601980f39808c9c33f823981203e9877
Simon Høxbro Hansen <hoxbro@protonmail.com>
2025-06-27 17:00:00 +0200

    fix: Duplicate markers (#50)

--- src/hv_anndata/plotting.py
+++ src/hv_anndata/plotting.py
@@ -87,8 +87,10 @@ class Dotmap(param.ParameterizedFunction):
     )
 
     def _prepare_data(self) -> pd.DataFrame:  # noqa: C901, PLR0912, PLR0915
-        # Flatten the marker_genes preserving order and duplicates
-        all_marker_genes = list(chain.from_iterable(self.p.marker_genes.values()))
+        # Flatten the marker_genes preserving order
+        all_marker_genes = list(
+            dict.fromkeys(chain.from_iterable(self.p.marker_genes.values()))
+        )
 
         # Determine to use raw or processed
         use_raw = self.p.use_raw
