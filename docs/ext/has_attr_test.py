b7d73ccd5c42bea2decb0789e37727f33953982d
Philipp A. <flying-sheep@web.de>
2026-06-05 14:56:57 -0400

    docs: fix inherited methods (#151)

--- docs/ext/has_attr_test.py
+++ docs/ext/has_attr_test.py
@@ -1,4 +1,7 @@
-"""Extension adding a jinja2 filter that tests if an object has an attribute."""
+"""Extension adding a jinja2 filter that tests if an object has an attribute.
+
+See <https://jinja.palletsprojects.com/en/3.0.x/api/#custom-tests>.
+"""
 
 from __future__ import annotations
 
@@ -14,12 +17,23 @@ if TYPE_CHECKING:
 
 def has_member(obj_path: str, attr: str) -> bool:
     """Test if an object has an attribute."""
-    # https://jinja.palletsprojects.com/en/3.0.x/api/#custom-tests
     obj = import_string(obj_path)
     return hasattr(obj, attr) or attr in get_annotations(obj)
+
+
+def is_inherited(obj_path: str, attr: str) -> bool:
+    """Test if an object attribute is inherited."""
+    obj = import_string(obj_path)
+    typ = obj if isinstance(obj, type) else type(obj)
+    if getattr(getattr(typ, attr, None), "__override__", False):
+        return False  # we’re explicitly overriding it
+    return any(
+        hasattr(cls, attr) or attr in get_annotations(cls) for cls in obj.mro()[1:]
+    )
 
 
 def setup(app: Sphinx) -> None:
     """App setup hook."""
     del app
     DEFAULT_NAMESPACE["has_member"] = has_member
+    DEFAULT_NAMESPACE["is_inherited"] = is_inherited
